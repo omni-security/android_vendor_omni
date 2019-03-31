Unlegacy-Android/android_vendor_unlegacy

Import "make bootzip" task
- Based on:

  From: RobbieL811 <robbielancaster811@gmail.com>
  Date: Tue, 12 May 2015 11:51:19 -0400
  Subject: [PATCH] Add mka bootzip
  Change-Id: I89163aa85c5341d0260bcb393cfc16dc9ff8a0c7

  From: Andreas Blaesius <skate4life@gmx.de>
  Date: Tue, 11 Aug 2015 16:50:52 +0200
  Subject: [PATCH] Set metadata on bootzip

  Change-Id: I54cf171f0b2e46ece98fa89ce2a3972bf7a5127d

Move the task to our vendor to avoid unneeded commits on top of AOSP source.

Change-Id: Ie74901fb4303d0940720bded5b7c884773a21598

build/tasks/bootzip.mk
@@ -1,0 +1,34 @@
+# Copyright (C) 2017 Unlegacy-Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+ifneq ($(TARGET_PRODUCT),sdk)
+ifeq ($(filter generic%,$(TARGET_DEVICE)),)
+ifneq ($(TARGET_NO_KERNEL),true)
+ifneq ($(recovery_fstab),)
+
+BOOT_ZIP_FROM_IMAGE_SCRIPT := ./vendor/unlegacy/build/tools/releasetools/boot_flash_from_image
+KERNEL_PATH := $(TARGET_KERNEL_SOURCE)/arch/arm/configs/$(TARGET_KERNEL_CONFIG)
+
+.PHONY: bootzip
+
+bootzip: bootimage
+	$(BOOT_ZIP_FROM_IMAGE_SCRIPT) \
+	   $(recovery_fstab) \
+	   $(OUT) \
+	   $(TARGET_DEVICE)
+
+endif    # recovery_fstab is defined
+endif    # TARGET_NO_KERNEL != true
+endif    # TARGET_DEVICE != generic*
+endif    # TARGET_PRODUCT != sdk

omni-security/android_vendor_omni

build/tasks/bootzip.mk
@@ -1,0 +1,34 @@
+# Copyright (C) 2017 Unlegacy-Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+ifneq ($(TARGET_PRODUCT),sdk)
+ifeq ($(filter generic%,$(TARGET_DEVICE)),)
+ifneq ($(TARGET_NO_KERNEL),true)
+ifneq ($(recovery_fstab),)
+
+BOOT_ZIP_FROM_IMAGE_SCRIPT := ./vendor/omni/build/tools/releasetools/boot_flash_from_image
+KERNEL_PATH := $(TARGET_KERNEL_SOURCE)/arch/arm/configs/$(TARGET_KERNEL_CONFIG)
+
+.PHONY: bootzip
+
+bootzip: bootimage
+	$(BOOT_ZIP_FROM_IMAGE_SCRIPT) \
+	   $(recovery_fstab) \
+	   $(OUT) \
+	   $(TARGET_DEVICE)
+
+endif    # recovery_fstab is defined
+endif    # TARGET_NO_KERNEL != true
+endif    # TARGET_DEVICE != generic*
+endif    # TARGET_PRODUCT != sdk